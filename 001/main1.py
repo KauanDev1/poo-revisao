211ed190791db218ea9fdbb7312dd825e3394058
KauanDev1 <k4uangnom0@gmail.com>
2026-04-19 00:14:07 -0400

    add dinheiro

---
+++ 001/main1.py
@@ -0,0 +1,35 @@
+class Pessoa:
+    def __init__(self, nome, idade, dinheiro):
+        self.nome = str(nome)
+        self.idade = int(idade)
+        self.dinheiro = int(dinheiro)
+        
+
+
+    def niver(self):
+        self.idade += 1
+ 
+    def apresentar(self):
+        print(f"Olá, meu nome é {self.nome} e tenho {self.idade} anos e tenho {self.dinheiro}.")
+
+    def dinheiroDepositar(self, valor):
+        self.dinheiro = self.dinheiro + valor
+
+    
+        
+
+kauan = Pessoa("kauan", 18, 10)
+kauan.niver()
+kauan.niver()
+kauan.dinheiroDepositar(100)
+kauan.apresentar()
+
+print("====================================")
+
+ana = Pessoa("Ana", 18, 999)
+ana.dinheiroDepositar(9999999)
+ana.apresentar()
+
+
+
+
